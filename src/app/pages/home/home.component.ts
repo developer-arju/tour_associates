7dd087fbed349a94a27fadb52a22f1c7fbecfeab
Ramdev <ramdevnemathillam@gmail.com>
2025-03-18 22:20:02 +0530

    added navbar bg service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SearchLgComponent } from "./search-lg/search-lg.component";
 import { DestinationCardComponent } from '../../shared/component/destination-card/destination-card.component';
+import { NavbarService } from '../../shared/services/navbar.service';
 
 type Destination = {
   place: string,
@@ -14,7 +15,8 @@ type Destination = {
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  navService = inject(NavbarService);
   items: number[] = Array(10).map((_,idx) => idx+1)
   destinations: Destination[] = [
     {
@@ -63,4 +65,8 @@ export class HomeComponent {
       amenities: ["Flights", "Hotels", "Resorts"]
     },
   ]
+
+  ngOnInit(): void {
+    this.navService.setBg("transparent")
+  }
 }
---
+++ src/app/shared/services/navbar.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarService {
+  private bgSubject = new BehaviorSubject<string>("");
+  backgroundColor = this.bgSubject.asObservable();
+
+
+  setBg(color: string) {
+    this.bgSubject.next(color)
+  }
+}
